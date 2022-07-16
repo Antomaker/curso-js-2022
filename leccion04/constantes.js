a7f6dd9d13afbec06176501ea1b835d992489ad0
Antomaker <jocdelavalle@gmail.com>
2022-07-15 20:34:20 -0500

    avance150722

---
+++ leccion04/constantes.js
@@ -0,0 +1,9 @@
+'use strict';
+var numero=20;
+
+var numero=40;
+
+const numero2=10;
+
+console.log(numero);
+console.log(numero2);
---
+++ leccion04/varylet.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var numero=20;
+var texto="hola";
+
+if(true){
+    let numero=40;
+    let texto="hola que hace";
+    console.log(numero);
+    console.log(texto);
+}
+
+console.log(numero);
+console.log(texto);
